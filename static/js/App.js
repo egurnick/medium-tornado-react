edca396aa7f7ab8dda0b2cb637ce86dff4c13afc
egurnick <evangurnick@gmail.com>
2023-03-01 14:20:52 -0800

    Add tutorial files

---
+++ static/js/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return(
+            <div className="content-container">
+                <p>Hello, from the world of react!</p>
+            </div>
+        );
+    }
+}
+export default App;
---
+++ static/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: 'static/dist',
+    },
+    devtool: 'source-map',
+    devServer: {
+        port: 8001,
+        proxy: {
+            '/': 'http://localhost:8000',
+        },        
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+}
